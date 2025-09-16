98918f16ade838821ad6683e0a6548179ef2f998
Hayoung <153509822+billihazero@users.noreply.github.com>
2025-09-16 09:46:22 +0900

    [Bronze II] Title: 문자열 반복, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/2675. 문자열 반복/문자열 반복.py
+++ 백준/Bronze/2675. 문자열 반복/문자열 반복.py
@@ -1,9 +1,5 @@
-N= int(input())
-for i in range (N):
-    a,b =input().split()
-    a = int(a)
-    s = len(b) # b의 길이
-    total = []
-    for x in range(s):
-        total += b[x] * a
-    print(''.join(total))
+N = int(input())
+for _ in range(N): 
+    a, str = input().split()
+    a=int(a)
+    print(''.join(ch * a for ch in str))

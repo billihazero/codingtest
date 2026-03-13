93ad8aee2a50719ffb5f5bcb6f838395816a57e4
billihazero <153509822+billihazero@users.noreply.github.com>
2026-03-13 14:12:04 +0900

    [Bronze III] Title: 최댓값, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/2562. 최댓값/최댓값.py
+++ 백준/Bronze/2562. 최댓값/최댓값.py
@@ -1,8 +1,7 @@
-nums = []
-i = 9
-while i !=0 :
-    nums.append(int(input()))
-    i -= 1
-a = max(nums)
-print(a)
-print(nums.index(a)+1)
+v = []
+for _ in range(9):
+    v.append(int(input()))
+
+max_here = v.index(max(v))
+print(v[max_here])
+print(max_here+1)
